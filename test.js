bfa75e18413e00c326a4b9bf491dd52b6bc5ecba
paul-ol1 <oladelepaul3@gmail.com>
2023-03-18 15:49:01 +0100

    commit

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+
 let x = fourw;
 let newwords=[];
 for(let i =0;i<x.length;i++){
@@ -21,3 +22,32 @@ for(let i =0;i<x.length;i++){
         }
     }
 }
+
+let allpossibilities = [];
+for(let i =0;i<x.length;i++){
+    let currentword = x[i];
+    let arr = [];
+    arr.push(currentword);
+    for(let z =0; z<newwords.length;z++){
+        if(newwords[z].word== currentword){
+            arr= arr.concat(newwords[z].next);
+            break;
+        }
+    }
+    allpossibilities.push(arr);
+    
+}
+for(let i=0;i<allpossibilities.length;i++){
+    for(let a =1;a<allpossibilities[i].length;a++){
+        for(let y =0; y<newwords.length;y++){
+            if(newwords[y].word== allpossibilities[i][a]){
+                for(let b =0;b<newwords[y].next.length;b++){
+                    if(!allpossibilities[i].includes(newwords[y].next[b])){
+                        allpossibilities[i].push(newwords[y].next[b]);
+                    }
+                }
+            break;
+        }
+    }
+    }}
+    console.log(allpossibilities);
